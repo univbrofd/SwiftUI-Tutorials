a358afb19ca2460c0127645da343e9b09d2fb021
AnaZafra <heiaanoalien@gmail.com>
2025-10-24 20:51:27 +0900

    Landmark作成

---
+++ Complete/Landmarks/circle.swift
@@ -0,0 +1,19 @@
+//
+//  circle.swift
+//  Landmarks
+//
+//  Created by Zafra Ana on R 7/10/24.
+//  Copyright © 7 Reiwa Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct circle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    circle()
+}
