7fb71344687d1a4ff7c29f386614f620ff44f1f7
Asaka Ko <hoodeight@icloud.com>
2025-10-24 17:40:21 +0900

    .

---
+++ Complete/Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A view showing the details for a landmark.
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
